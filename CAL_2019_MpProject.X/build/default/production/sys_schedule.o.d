cf318fa9bc4db87f17482d176424931c9ca02a4f
unknown <stefanboghean98@gmail.com>
2019-11-06 19:36:18 +0200

    Added MpLab Project BoilerPlate

---
+++ CAL_2019_MpProject.X/build/default/production/sys_schedule.o.d
@@ -0,0 +1,8 @@
+build/default/production/sys_schedule.o:  \
+sys_schedule.c  \
+general.h  \
+ general_types.h  \
+mcal_gpio.h  \
+sys_tasks.h  \
+mcal_timers.h  \
+sys_schedule.h 
---
+++ CAL_2019_MpProject.X/build/default/production/sys_tasks.o.d
@@ -0,0 +1,15 @@
+build/default/production/sys_tasks.o:  \
+sys_tasks.c  \
+general.h  \
+ general_types.h  \
+mcal_gpio.h  \
+sys_tasks.h  \
+mcal_init.h  \
+mcal_osc.h  \
+ mcal_adc.h  \
+mcal_pwm.h  \
+mcal_interrupts.h  \
+mcal_spi.h  \
+mcal_timers.h  \
+ mcal_encoder.h  \
+asw_com.h 
